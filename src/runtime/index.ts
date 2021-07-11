6630e2e9d79bfac6c5afb78a5c74a7c8f0e8e35f
Daniel Roe <daniel@roe.dev>
2021-07-11 11:26:16 +0100

    refactor: combine into single function

--- src/runtime/index.ts
+++ src/runtime/index.ts
@@ -1,13 +1,9 @@
 import chalk from 'chalk'
 
 export function sanitizeString (string: string) {
-  return removeInvalidChars(string.replace(/\//g, '-'))
-}
-
-export function removeInvalidChars (string: string) {
-  return string.replace(/(\/|:)/g, '-')
+  return string.replace(/[/:]/g, '-')
 }
 
 export function prettyTimer (method: 'time' | 'timeEnd', lifecycle: string, process: string, name: string) {
-  console[method](`${chalk.greenBright(lifecycle.padEnd(10))} ${chalk.grey('>>')} ${process.padEnd(10)}  ${chalk.bold(removeInvalidChars(name).padEnd(30))}`)
+  console[method](`${chalk.greenBright(lifecycle.padEnd(10))} ${chalk.grey('>>')} ${process.padEnd(10)}  ${chalk.bold(sanitizeString(name).padEnd(30))}`)
 }
